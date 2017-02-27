7cd180cb0a5002f60932b01a86a876e26b361078
modesty-git <passey.185@gmail.com>
2017-02-27 19:54:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // move the image
-var img = documment.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function(){
 img.style.marginleft = '100px';
 };
